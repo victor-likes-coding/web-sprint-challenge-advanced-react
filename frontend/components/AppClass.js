1bb3e18f748971d171f7d83375ed2c6a999279d1
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-19 13:01:58 -0500

    update coordinate to use helper fn's

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -32,7 +32,7 @@ export default class AppClass extends React.Component {
                 id="wrapper"
                 className={className}>
                 <div className="info">
-                    <h3 id="coordinates">Coordinates (2, 2)</h3>
+                    <h3 id="coordinates">Coordinates {this.getCoordinates()}</h3>
                     <h3 id="steps">You moved 0 times</h3>
                 </div>
                 <div id="grid">
