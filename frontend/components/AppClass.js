fff70a7cdbc6ccec14dbbcbf21c39fc477794b3a
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-19 13:02:27 -0500

    update code to use this.state.activeSquare

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -39,8 +39,8 @@ export default class AppClass extends React.Component {
                     {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
                         <div
                             key={idx}
-                            className={`square${idx === 4 ? " active" : ""}`}>
-                            {idx === 4 ? "B" : null}
+                            className={`square${idx === this.state.activeSquare ? " active" : ""}`}>
+                            {idx === this.state.activeSquare ? "B" : null}
                         </div>
                     ))}
                 </div>
